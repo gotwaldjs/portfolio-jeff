5f44c8d141f1c807f857c989fcac5acf7baafa8c
jefegwald <jeffreygottwald@gmail.com>
2023-05-07 21:49:31 -0400

    build

--- components/header/header.js
+++ components/header/header.js
@@ -1,6 +1,6 @@
 // Fetch the header HTML code from the external file
 const xhr = new XMLHttpRequest();
-xhr.open('GET', 'header.html');
+xhr.open('GET', './header.html');
 xhr.onload = function() {
   if (xhr.status === 200) {
     // Insert the header code into the placeholder element
