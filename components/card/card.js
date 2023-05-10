6edc93a678307db8202020da91df8e21ef80e991
jefegwald <jeffreygottwald@gmail.com>
2023-05-09 21:22:42 -0400

    build

--- components/card/card.js
+++ components/card/card.js
@@ -5,6 +5,17 @@ class Card extends HTMLElement {
       // Create a shadow root for the custom element
       const shadowRoot = this.attachShadow({ mode: 'open' });
   
+      // Fetch the card HTML code from the external file
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', './components/card/card.html');
+      xhr.onload = function () {
+        if (xhr.status === 200) {
+          // Insert the card code into the shadow root
+          shadowRoot.innerHTML = xhr.responseText;
+        }
+      };
+      xhr.send();
+  
       // Get the card attributes
       const imageSrc = this.getAttribute('image-src');
       const title = this.getAttribute('title');
@@ -15,51 +26,23 @@ class Card extends HTMLElement {
       const tagLabel = this.getAttribute('tag-label');
   
       // Create the card HTML structure
-      const cardContainer = document.createElement('div');
-      cardContainer.classList.add('card');
+      const cardContainer = shadowRoot.querySelector('.card');
       cardContainer.style.backgroundColor = backgroundColor;
   
-      const cardImage = document.createElement('img');
+      const cardImage = shadowRoot.querySelector('.card__image');
       cardImage.src = imageSrc;
-      cardImage.alt = '';
-      cardImage.classList.add('card__image');
-      cardContainer.appendChild(cardImage);
   
-      const cardDescription = document.createElement('div');
-      cardDescription.classList.add('addBorderBottom');
-      const cardDescriptionText = document.createElement('p');
-      cardDescriptionText.classList.add('card__description');
+      const cardDescriptionText = shadowRoot.querySelector('.card__description');
       cardDescriptionText.textContent = description;
-      cardDescription.appendChild(cardDescriptionText);
-      cardContainer.appendChild(cardDescription);
   
-      const cardFooter = document.createElement('div');
-      cardFooter.id = 'card__footer';
-      cardFooter.style.display = 'flex';
-
-      
-
-
-
-      cardFooter.style.justifyContent = 'space-between';
-      const cardTag = document.createElement('carbon-tag');
-      cardTag.setAttribute('size', 'sm');
+      const cardFooter = shadowRoot.querySelector('#card__footer');
+      const cardTag = cardFooter.querySelector('carbon-tag');
       cardTag.setAttribute('type', tagType);
-      cardTag.title = 'Clear Filter';
       cardTag.textContent = tagLabel;
-      cardFooter.appendChild(cardTag);
-      cardContainer.appendChild(cardFooter);
   
       // Create a link to the CTA
-      const cardLink = document.createElement('a');
+      const cardLink = shadowRoot.querySelector('.card__link');
       cardLink.href = ctaLink;
-      cardLink.style.display = 'block';
-      cardLink.style.height = '100%';
-      cardLink.style.width = '100%';
-      cardContainer.appendChild(cardLink);
-  
-      // Attach the card HTML structure to the shadow root
-      shadowRoot.appendChild(cardContainer);
   
       // Add the CSS styles to the shadow root
       const style = document.createElement('style');
